f121a8f8a1013e829c4ce36b7275b85d42986462
jannick vandaele <jannickvandaele@gmail.com>
2020-03-30 20:44:41 +0200

    Updates

---
+++ precache-manifest.35be8e8f57be12e7653f84d22bfa776e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "527f92573b8c2eb3683c7b6f6d63e4b0",
+    "url": "/github-final/index.html"
+  },
+  {
+    "revision": "3aa709f10b37b031550b",
+    "url": "/github-final/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4f77267142070f152c27",
+    "url": "/github-final/static/js/2.93b77137.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-final/static/js/2.93b77137.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3aa709f10b37b031550b",
+    "url": "/github-final/static/js/main.318922e8.chunk.js"
+  },
+  {
+    "revision": "10a298cd903b6bfe1e26",
+    "url": "/github-final/static/js/runtime-main.06089012.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-final/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b1b6ddc62675ba35e5cd96be8a433e7b.js"
+  "/github-final/precache-manifest.35be8e8f57be12e7653f84d22bfa776e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github-final/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
